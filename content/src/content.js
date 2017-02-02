5c045b6dd6ad488163144d69290afcb53eeed255
Winston <winston.j.zhao@gmail.com>
2017-02-01 18:45:25 -0800

    clean up functions move to mutation-observer.js file

--- content/src/content.js
+++ content/src/content.js
@@ -1,28 +1,14 @@
 import { getWord, getFieldsAndAddEventListeners } from './listener';
+import { mutationObserver } from './mutation-observer';
+
 console.log("JavaScript successfully injected");
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-var observer = new MutationObserver(function(mutations, observer) {
-    // fired when a mutation occurs
-    getFieldsAndAddEventListeners('div[contenteditable="true"]');
-    getFieldsAndAddEventListeners('input');
-    getFieldsAndAddEventListeners('textarea');
-    // ...
-});
-
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
-observer.observe(document, {
-  subtree: true,
-  attributes: true
-  //...
-});
+mutationObserver();
 
 (() => {
     const anchor = document.createElement('div');
     anchor.id = 'upword-anchor';
     document.body.insertBefore(anchor, document.body.childNodes[0]);
-    document.body.addEventListener('mouseup', getWord);
+    // document.body.addEventListener('mouseup', getWord);
 
 })();
