c796665b69125e29fa3c34e745a5bc8e74636a30
Winston <winston.j.zhao@gmail.com>
2017-02-16 15:34:03 -0800

    dropdown

--- content/src/dropdown/dropdown.js
+++ content/src/dropdown/dropdown.js
@@ -7,8 +7,27 @@ const createDropdown = (wordInfo, selectionCoordinates) => {
     upwordDropdown.style.left = selectionCoordinates.left + "px";
     upwordDropdown.style.width = 100 + "px";
     upwordDropdown.style.height = 100 + "px";
-
+    for (let partOfSpeech in wordInfo) {
+        if (partOfSpeech !== 'word') {
+            let list = createList(partOfSpeech, wordInfo[partOfSpeech]);
+            upwordDropdown.appendChild(list);
+        }
+    }
     return upwordDropdown;
+}
+
+const createList = (partOfSpeech, synonyms) => {
+    let partOfSpeechDiv = document.createElement('div');
+    partOfSpeechDiv.setAttribute('id', partOfSpeech);
+    partOfSpeechDiv.setAttribute('class', 'part-of-speech');
+    partOfSpeechDiv.innerHTML = partOfSpeech;
+    partOfSpeechDiv.addEventListener('click', function() {
+        console.log(this);
+    });
+    partOfSpeechDiv.style.width = 100 + "px";
+    partOfSpeechDiv.style.width = 25 + "px";
+
+    return partOfSpeechDiv;
 }
 
 export default createDropdown;
