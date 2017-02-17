a4410aa97c8b2b6f591256155a81ceef4c5fd0d3
Winston <winston.j.zhao@gmail.com>
2017-02-16 23:35:20 -0800

    working on creating list

--- content/src/dropdown/dropdown.js
+++ content/src/dropdown/dropdown.js
@@ -1,33 +1,23 @@
+import createDropdownTabsAndInfo from './dropdown-tabs';
+
 const createDropdown = (wordInfo, selectionCoordinates) => {
     console.log(wordInfo);
     let upwordDropdown = document.createElement('div');
 
     upwordDropdown.setAttribute('class', 'upword-dropdown');
-    upwordDropdown.style.top = selectionCoordinates.bottom + "px";
+    // if (selectionCoordinates.bottom) {
+    upwordDropdown.style.top = window.pageYOffset + selectionCoordinates.bottom + "px";
+    // } else {
+        // upwordDropdown.style.top = window.pageYOffset + selectionCoordinates.top + "px";
+    // }
     upwordDropdown.style.left = selectionCoordinates.left + "px";
-    upwordDropdown.style.width = 100 + "px";
     upwordDropdown.style.height = 100 + "px";
-    for (let partOfSpeech in wordInfo) {
-        if (partOfSpeech !== 'word') {
-            let list = createList(partOfSpeech, wordInfo[partOfSpeech]);
-            upwordDropdown.appendChild(list);
-        }
-    }
+    upwordDropdown.style.width = 100 + "px";
+
+    let dropdownButtons = createDropdownTabsAndInfo(upwordDropdown, wordInfo);
+
+    upwordDropdown.appendChild(dropdownButtons);
     return upwordDropdown;
-}
-
-const createList = (partOfSpeech, synonyms) => {
-    let partOfSpeechDiv = document.createElement('div');
-    partOfSpeechDiv.setAttribute('id', partOfSpeech);
-    partOfSpeechDiv.setAttribute('class', 'part-of-speech');
-    partOfSpeechDiv.innerHTML = partOfSpeech;
-    partOfSpeechDiv.addEventListener('click', function() {
-        console.log(this);
-    });
-    partOfSpeechDiv.style.width = 100 + "px";
-    partOfSpeechDiv.style.width = 25 + "px";
-
-    return partOfSpeechDiv;
 }
 
 export default createDropdown;
--- content/src/listener.js
+++ content/src/listener.js
@@ -7,13 +7,20 @@ export const getWord = () => {
               document.selection.createRange();
 
     let selection = txt.toString().toLowerCase();
-    let selectionCoordinates = txt.getRangeAt(0).getBoundingClientRect();
-
+    console.log(selection);
+    // if (selection) {
+    //     selectionCoordinates = getTextBoundingRect();
+    //     console.log(selectionCoordinates);
+    // }
+    //
     let validSelection = selection.trim().length > 0 ? true : false;
     if (validSelection) {
+        let selectionCoordinates = txt.getRangeAt(0).getBoundingClientRect();
+        if (!selectionCoordinates.bottom) {
+            selectionCoordinates = getTextBoundingRect();
+        }
         let url = `https://upword-server.herokuapp.com/word/${selection}`;
         axios.get(url).then(function(response){
-            console.log(response.data);
             let upwordAnchor = document.getElementById('upword-anchor');
             let upwordDropdown = createDropdown(response.data, selectionCoordinates);
             console.log(upwordDropdown);
@@ -34,4 +41,50 @@ export const getFieldsAndAddEventListeners = (queryString) => {
             field.addEventListener(event, getWord);
         });
     }
+}
+
+var getTextBoundingRect = function() {
+    var sel = document.selection, range, rect;
+    var x = 0, y = 0;
+    if (sel) {
+        if (sel.type != "Control") {
+            range = sel.createRange();
+            range.collapse(true);
+            x = range.boundingLeft;
+            y = range.boundingTop;
+        }
+    } else if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0).cloneRange();
+            if (range.getClientRects) {
+                range.collapse(true);
+                if (range.getClientRects().length>0){
+                    rect = range.getClientRects()[0];
+                    x = rect.left;
+                    y = rect.top;
+                }
+            }
+            // Fall back to inserting a temporary element
+            if (x == 0 && y == 0) {
+                var span = document.createElement("span");
+                if (span.getClientRects) {
+                    // Ensure span has dimensions and position by
+                    // adding a zero-width space character
+                    span.appendChild( document.createTextNode("\u200b") );
+                    range.insertNode(span);
+                    rect = span.getClientRects()[0];
+                    x = rect.left;
+                    y = rect.top;
+                    var spanParent = span.parentNode;
+                    spanParent.removeChild(span);
+
+                    // Glue any broken text nodes back together
+                    spanParent.normalize();
+                }
+            }
+        }
+    }
+    console.log(x, y);
+    return { left: x, top: y };
 }
